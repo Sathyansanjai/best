8645eaee09581fab5f960733dc7ba48cef403a31
Sathyansanjai <Sathyansanjai007@gmail.com>
2025-02-25 18:36:04 +0530

    it an new entrollment commit all file

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("quotes");
+function boxx() {
+  quotes.style.boxShadow = "15px 15px 10px rgb(22, 8, 85)";
+  quotes.style.backgroundImage =
+    "linear-gradient(rgba(36, 37, 25, 0.726), rgba(231, 211, 211, 0.87))";
+}
+
+function boxleave() {
+  quotes.style.boxShadow = "10px 10px 8px rgb(0, 0, 0)";
+  quotes.style.backgroundImage =
+    "linear-gradient( rgba(51, 75, 104, 0.925), rgba(225, 219, 226, 0.781))";
+}
